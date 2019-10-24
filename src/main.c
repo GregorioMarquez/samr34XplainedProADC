ddf1fd7d4aae50531ed74325b6e418339798d628
Gregorio Marquez <gregorio.marquezm@gmail.com>
2019-10-24 00:41:55 -0500

    first commit

---
+++ src/main.c
@@ -0,0 +1,65 @@
+/**
+ * \file
+ *
+ * \brief Empty user application template
+ *
+ */
+
+/**
+ * \mainpage User Application template doxygen documentation
+ *
+ * \par Empty user application template
+ *
+ * This is a bare minimum user application template.
+ *
+ * For documentation of the board, go \ref group_common_boards "here" for a link
+ * to the board-specific documentation.
+ *
+ * \par Content
+ *
+ * -# Include the ASF header files (through asf.h)
+ * -# Minimal main function that starts with a call to system_init()
+ * -# Basic usage of on-board LED and button
+ * -# "Insert application code here" comment
+ *
+ */
+
+/*
+ * Include header files for all drivers that have been imported from
+ * Atmel Software Framework (ASF).
+ */
+/*
+ * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
+ */
+#include <asf.h>
+#include <adc.h>
+
+struct adc_module adc_instance;
+
+void configure_adc(void);
+
+int main (void)
+{
+	system_init();
+
+	/* Insert application code here, after the board has been initialized. */
+	
+	configure_adc();		
+	
+	while (1) {
+		adc_start_conversion(&adc_instance);
+		uint16_t result;
+		do {
+		} while (adc_read(&adc_instance, &result) == STATUS_BUSY);
+		
+	}
+	
+}
+
+void configure_adc(void)
+{
+	struct adc_config config_adc;
+	adc_get_config_defaults(&config_adc);
+	adc_init(&adc_instance, ADC, &config_adc);
+	adc_enable(&adc_instance);
+}
